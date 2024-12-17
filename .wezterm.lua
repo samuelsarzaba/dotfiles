d785cf5159ec935d7415bf42c5962ea195b4ddf9
samuelsarzaba <samuelsarzaba@gmail.com>
2024-12-16 22:48:03 -0500

    Removed starship config

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,9 +5,13 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.font = wezterm.font 'JetBrainsMono Nerd Font Mono'
+
+config.color_scheme = 'Catppuccin Mocha'
+
+config.enable_tab_bar = false
+
+config.window_decorations = "NONE"
 
 -- and finally, return the configuration to wezterm
 return config
